c7fae7f3f7c7c25a0116e2edc57025ea437ce4c0
Burak Imre <burakimre1999@gmail.com>
2022-11-26 20:39:47 +0100

    added descriptive information about playlist in banner component

--- client/src/components/Banner.tsx
+++ client/src/components/Banner.tsx
@@ -7,16 +7,16 @@ function Banner(props: any) {
                 <img className="w-full h-full object-cover rounded-xl blur-lg" src={ props.src } alt=""/>
                 <div className="absolute top-0 w-full h-full bg-black/40"></div>
             </div>
-            <div className="flex justify-center lg:justify-start space-x-8 w-full p-4 rounded-xl z-10">
+            <div className="flex flex-col lg:flex-row justify-center lg:justify-start items-center lg:items-stretch space-x-0 lg:space-x-8 space-y-2 lg:space-y-0 w-full p-4 rounded-xl z-10">
                 <img className="w-36 h-36 lg:w-56 lg:h-56 rounded-xl" src={ props.src } alt=""/>
-                <div className="hidden lg:flex flex-col justify-center">
+                <div className="flex flex-col justify-center">
                     <div className="flex-1"></div>
                     <div className="flex flex-col">
-                        <span className="text-white text-6xl font-bold">{ props.name }</span>
-                        <span className="text-gray-300 text-md">{ props.description }</span>
+                        <span className="text-white text-md lg:text-6xl font-bold">{ props.name }</span>
+                        <span className="text-gray-300 text-sm lg:text-md">{ props.description }</span>
                     </div>
-                    <div className="flex flex-1">
-                        <span className="self-end text-white text-md">{ props.total + " " + (props.total == 1 ? "track" : "tracks") }</span>
+                    <div className="flex flex-1 justify-center">
+                        <span className="self-end text-gray-400 lg:text-white text-md">{ props.total + " " + (props.total == 1 ? "track" : "tracks") }</span>
                     </div>
                 </div>
             </div>
